6552f06bdecb82c7d25b7278cf4e79b6b7c847e5
Caio Paula <caio.paula@sou.inteli.edu.br>
2023-07-18 14:38:39 -0300

    Adding contents

--- public/js/partials/sidebar.js
+++ public/js/partials/sidebar.js
@@ -1,3 +1,15 @@
-function openSideBar(){
-    document.querySelector("")
+let sideBar = document.querySelector("aside.side_bar_nav");
+var vision = false
+
+function toggleSideBar(){
+    
+
+    if(vision == false){
+        sideBar.style.width = "100%";
+        vision = true
+    }else if(vision == true){
+        sideBar.style.width = "0%"
+        vision = false
+        
+    }
 }
